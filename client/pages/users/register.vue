1e16a65677088420de1060151893968412f7c93d
adakichi <l10yusukeadachi@yahoo.co.jp>
2021-11-29 20:29:35 +0900

    新規登録Vue追加

---
+++ client/pages/users/register.vue
@@ -0,0 +1,59 @@
+<template>
+<v-container>
+    <v-row justify="center" aligh-content="center">
+        <v-col col=9 xs=9 sm=9 md=6 lg=6>
+            <v-card>
+              <v-card-title>
+                新規ユーザー登録
+                </v-card-title>
+                <v-form ref="form" @submit.prevent="">
+                <v-card-text>
+                    <v-text-field
+                      v-model="user.userId"
+                      :counter="20"
+                      label="ID"
+                      clearable
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="user.password"
+                      :append-icon="showpass ? 'mdi-eye' : 'mdi-eye-off' "
+                      :type=" showpass ? 'text' : 'password' "
+                      label="PASSWORD"
+                      required
+                      @click:append="showpass = !showpass"
+                    ></v-text-field>
+                    <v-btn @click="createUser">登録</v-btn>
+                    </v-card-text>
+                </v-form>
+            </v-card>
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+    data(){
+      return {
+        user:{
+          userId:'',
+          password:''
+        },
+        showpass:false
+      }
+    },
+    methods:{
+      createUser(){
+        console.log('--post create user---')
+        const data = {name:this.name,password:this.password}
+        axios.post('http://localhost:3000/api/auth/register/', data)
+        .then((res)=>{
+          if(res.data.message){alert(res.data.message)}
+            this.$auth.loginWith('local',{data:this.user})
+        })
+      }
+    }
+  }
+</script>
