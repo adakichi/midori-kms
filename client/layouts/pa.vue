66712b8198080f7fbf53fc03359bea0bd321d1a0
adakichi <l10yusukeadachi@yahoo.co.jp>
2021-12-03 12:47:24 +0900

    payment_agency追加

--- client/layouts/pa.vue
+++ client/layouts/pa.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -53,24 +53,6 @@
       <v-spacer />
             User:{{$auth.user ? $auth.user.name : 'ゲスト'}}
       <v-spacer />
-      <v-menu offset-y dense>
-        <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >内線表</v-btn>
-        </template>
-        <v-list nav>
-          <v-list-group
-          v-for="group in phonebook" :key="group.title">
-            <v-list-item-title>{{group.title}}</v-list-item-title>
-          </v-list-group>
-          </v-list>
-      </v-menu>
-      <v-btn v-if="!$auth.loggedIn" color="primary" @click="$router.push('/users/login')">ログイン</v-btn>
-      <v-btn v-else @click="$auth.logout()">ログアウト</v-btn>
         <theme-toggle></theme-toggle>
       <v-btn
         icon
@@ -119,44 +101,34 @@ export default {
     return {
       phonebook:phonejson.book,
       clipped: false,
-      drawer: false,
+      drawer: true,
       fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Back',
           to: '/'
         },
         {
-          icon: 'mdi-login',
-          title: 'Login',
-          to: '/users/login'
+          icon: 'mdi-cash-fast',
+          title: '入金予定',
+          to: '/come_in_schedule/'
         },
         {
           icon: 'mdi-cash-fast',
-          title: '代行支払い',
-          to: '/payment_agency'
+          title: '入金実績',
+          to: '/come_in_record/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-cash-fast',
+          title: '入金予定',
+          to: '/put_out_record/'
         },
         {
-          icon: 'mdi-tools',
-          title: 'Tools',
-          to: '/tools/tools_index'
+          icon: 'mdi-account',
+          title: '顧客毎',
+          to: '/customer/'
         },
-        {
-          icon: 'mdi-school',
-          title: 'Study',
-          to: '/study/study_index'
-        },
-        {
-          icon: 'mdi-frequently-asked-questions',
-          title: 'FAQ',
-          to: '/faq/faq_index'
-        }
       ],
       miniVariant: false,
       right: true,
---
+++ client/pages/payment_agency/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h1>代行支払い</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    layout : 'pa',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
