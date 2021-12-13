ca887ed510214e7ebfe2c83fec94c83352475bae
adakichi <l10yusukeadachi@yahoo.co.jp>
2021-12-13 23:07:47 +0900

    directory構造変更

---
+++ client/pages/payment_agency/customers/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-app-bar>
+                    <v-app-bar-title>Customers</v-app-bar-title>
+                    <v-spacer></v-spacer>
+                    <v-text-field v-model="targetText" label="search"></v-text-field>
+                    <v-btn @click="searchCustomer">検索</v-btn>
+                </v-app-bar>
+                <v-data-table
+                :headers="headers"
+                :items="customers"
+                item-key="customer_id"
+                :items-per-page="10"
+                class="elevation-1"
+                :search="search"
+                show-select
+                show-group-by
+                dense
+                @click:row="goCustomerPage"
+                >
+                <template v-slot:top>
+                <v-text-field
+                v-model="search"
+                label="Search(まだうごかんばい)"
+                class="mx-4"
+                >
+                </v-text-field>
+                </template>
+                    <template v-slot:item.delete_flag="{item}">
+                        <v-simple-checkbox
+                        v-model="item.delete_flag === 0 ?false:true"
+                        disabled
+                        >
+                        </v-simple-checkbox>
+                    </template>
+                </v-data-table>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    layout : 'pa',
+    data(){
+        return{
+            targetText:'',
+            search:'',
+            headers:[
+                { text:'customer_id', value:'customer_id'},
+                { text:'name', value:'name'}
+            ]
+        }
+    },
+    computed:{
+        customers(){
+            return this.$store.getters['pa/getCustomers']
+        }
+    },
+    methods:{
+        searchCustomer(){
+            this.$store.dispatch('pa/searchCustomers',this.targetText)
+        },
+        goCustomerPage(e){
+            console.log(e)
+            // this.$router.push('/payment_agency/customers/'+e.customer_id)
+        }
+    }
+}
+</script>
