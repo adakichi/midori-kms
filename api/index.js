9bf004c4bf73a29979c193c99fdd28a6dd215e40
adakichi <l10yusukeadachi@yahoo.co.jp>
2022-01-04 13:11:18 +0900

    logout後loginページへリダイレクト

--- api/index.js
+++ api/index.js
@@ -110,9 +110,8 @@ app.post('/auth/login',(req,res)=>{
 
 //ログアウト後の動作
   app.post('/auth/logout',(req,res)=>{
-    console.log(req)
     console.log('\n--- post /auth/logout/ ---\n' + moment().format('YYYY/MM/DD hh:mm:ss')+ '>' + req.body.auth + ' がログアウトしました。\n--- --- --- ---')
-    res.send('OK')
+    res.redirect('/user/login')
   })
   
 //新規登録
