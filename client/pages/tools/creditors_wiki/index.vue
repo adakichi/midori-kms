26c413f97e01e86c1255fc0eed432440f3fecbdd
adakichi <l10yusukeadachi@yahoo.co.jp>
2022-03-11 12:38:28 +0900

    基本ページ作成＆tools indexに追加

---
+++ client/pages/tools/creditors_wiki/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h1>掲示板</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+        }
+    },
+    methods:{
+        switchDialog(){
+            this.dialog = !this.dialog
+        },
+    },
+    computed:{
+    },
+    created(){
+    }
+}
+</script>
