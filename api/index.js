716e9562c8172cc500b9bad8ed05b3275025573d
adakichi <l10yusukeadachi@yahoo.co.jp>
2022-03-17 19:17:43 +0900

    DB.midori_usersをmkmsから分離したことでboardsにエラーがあったので修正

--- api/index.js
+++ api/index.js
@@ -2122,8 +2122,8 @@ app.post('/issues/',(req,res)=>{
 app.get('/issue',(req,res)=>{
   const id = parseInt(req.query.id,10)
   console.log('\n--- get /issues/ id:' + id + '---')
-  let sql = 'SELECT msg.*, name from issues_messages as msg '
-      sql = sql + 'inner join users on msg.author = users.user_id '
+  let sql = 'SELECT msg.*, name from mkms.issues_messages as msg '
+      sql = sql + 'inner join midori_users.users on msg.author = users.user_id '
       sql = sql + 'WHERE issue_id = ? ORDER BY msg.created_at;'
   db_mkms.query(sql,id,(err,rows,fields)=>{
     if(err){ err.whichApi= 'get /issue' ; throw err }
