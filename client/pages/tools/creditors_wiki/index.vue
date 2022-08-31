0a93d8bf15432f17aeee03f368f351322cd9dc27
adakichi <l10yusukeadachi@yahoo.co.jp>
2022-08-31 10:17:16 +0900

    債権者wiki 受任通知郵送？表示を修正

--- client/pages/tools/creditors_wiki/index.vue
+++ client/pages/tools/creditors_wiki/index.vue
@@ -689,7 +689,7 @@
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>{{selectedCreditor.mail_start === 1 ? '郵送': 'FAX'}}</td>
+                                            <td>{{whichStart}}</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -987,7 +987,13 @@ export default {
             accureedString:['必要','不要','必要(提案まで)','必要(和解日まで)','必要(支払開始まで)','必要(12回であれば不要)'],
             interestString:['必要','不要','減率'],
             contractCreator:['債権者','事務所'],
-            mailStart:[{text:'FAXでOK',value:0},{text:'郵送',value:1},{text:'メモ参照',value:2},{text:'未選択',value:null}],
+            mailStart:[
+                {text:'FAXでOK',    value:1},
+                {text:'郵送',       value:2},
+                {text:'メモ参照',   value:3},
+                {text:'未確認',     value:0},
+                {text:'データ無し',     value:null}
+                ],
             /////////////////////////////
             shortTerm:[
                 {text:'罰有り',value:1},
@@ -1135,6 +1141,24 @@ export default {
     computed:{
         themeIsDark(){
             return this.$vuetify.theme.isDark
+        },
+        whichStart(){
+            let returnString = "未選択"
+            switch(this.selectedCreditor.mail_start){
+                case 0:
+                    returnString = "未確認"
+                break
+                case 1:
+                    returnString = "FAXでOK"
+                break
+                case 2:
+                    returnString = "郵送"
+                break
+                case 3:
+                    returnString = "メモ参照"
+                break
+            }
+            return returnString
         }
     },
     created(){
